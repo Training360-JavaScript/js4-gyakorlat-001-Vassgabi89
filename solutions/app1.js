948faaa19ec584260f7f10a7dccd08d93c58dfad
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2021-11-22 19:46:35 +0100

    done

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,22 @@
+/*Írj egy olyan függvényt, amely paraméterként kap egy tömböt. A függvény visszatérési értéke egy Promise.  
+- A Promise resolved lesz, amennyiben a tömb minden eleme string, és azok sikeresen nagybetűssé alakíthatók: tehát a Promise visszaad 
+egy új tömböt, amely az eredeti tömb összes elemét nagybetűssé alakítva tartalmazza.
+- Ellenkező esetben a Promise rejected lesz, a visszatérési érték pontosan: 
+`Error: Not all elements are string type!`*/
 
+const converterPromise = (array) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let onlyStringInArray = true
+            array.forEach(item => {
+                if (!(typeof(item) === 'string')) {onlyStringInArray = false}
+            })
+            if (onlyStringInArray) {
+                let arrayUC = array.map(item => item.toUpperCase())
+                resolve(arrayUC)}
+            else {reject('Error: Not all elements are string type!')}
+        }, 0)
+    })
+}
 
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,22 @@
+/*- Írj egy függvényt, amely paraméterként szintén egy tömböt kap. A függvény visszatérési értéke egy Promise.
+- A Promise resolved lesz, amennyiben a tömb minden eleme string, és visszatér 
+ezek abc-rend szerint rendezett tömbjével.  
+- Ha nem minden elem string, akkor a Promise rejected lesz, visszatérési értéke:
+`Error: Not all elements are string type!`*/
 
+const sorterPromise = (array) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let onlyStringInArray = true
+            array.forEach(item => {
+                if (!(typeof(item) === 'string')) {onlyStringInArray = false}
+            })
+            if (onlyStringInArray) {
+                array.sort()
+                resolve(array)}
+            else {reject('Error: Not all elements are string type!')}
+        }, 0)
+    })
+}
 
-
-
-
-
-// export default sorterPromise;
+export default sorterPromise;
